9013bbe38839e1bdf28cd3913ef1947bf2e59b0f
Rick Clark <rkingsleyclark@gmail.com>
2017-02-07 12:20:33 +0000

    Created initial space model

--- app/dmconfig.rb
+++ app/dmconfig.rb
@@ -2,6 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 #REQUIRE MODELS HERE
 # require_relative 'models/user.rb'
+require_relative 'models/space.rb'
 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
---
+++ app/models/space.rb
@@ -0,0 +1,16 @@
+require 'dm-validations'
+
+class Space
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, required: true
+  property :description, Text, required: true
+  property :price, Integer, required: true
+  property :to, Date, required: true
+  property :from, Date, required: true
+
+  # belongs_to :user
+
+end
