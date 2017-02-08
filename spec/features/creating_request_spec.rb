51e4bd7a0c5a79ac92c8571cf6afa01b8066147c
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-02-08 11:47:45 +0000

    added feature tests to requests

--- spec/features/creating_request_spec.rb
+++ spec/features/creating_request_spec.rb
@@ -1,6 +1,14 @@
 feature 'FEATURE: Creating a request' do
+
+  let(:name) { "Pedro's House" }
+  let(:description) { "Very nice" }
+  let(:price) { 200 }
+  let(:from) { "20/01/2017" }
+  let(:to) { "20/02/2017" }
+
   xscenario 'logged in user is able to create a request' do
     sign_up
+    create_new_space(name: name, description: description, price: price, from: from, to: to)
     visit '/' #update later
     expect(page).to have_button("Request to Book")
   end
@@ -8,6 +16,16 @@ feature 'FEATURE: Creating a request' do
   xscenario 'when not logged in' do
     visit '/' #update later
     expect(page).not_to have_button("Request to Book")
+  end
+
+  xscenario 'logged in user is able to book' do
+    sign_up
+    create_new_space(name: name, description: description, price: price, from: from, to: to)
+    space_id = Space.first.id
+    visit "/spaces/#{space_id}" #update later
+    fill_in(:date, with: "31/01/2017")
+    click_button('Request to Book')
+    expect(Request.count).to change
   end
 
 end
