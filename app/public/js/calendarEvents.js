fca41ef34ddbabad56427958b9de2bc82afd3382
bvjones <benvaughanjones@gmail.com>
2017-02-08 16:54:39 +0000

    added datapicker to space page

---
+++ app/public/js/calendarEvents.js
@@ -0,0 +1,10 @@
+$ (document).ready(function(){
+  var date_input=$('input[name="date"]');
+  var options={
+    format: 'dd/mm/yyyy',
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
+
+  });
