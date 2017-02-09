b0fdf70a5d6dcbca64c578f122e3355ded0618e0
bvjones <benvaughanjones@gmail.com>
2017-02-09 14:39:12 +0000

    embedded datepicker on spaces/space page

--- app/public/js/calendarEvents.js
+++ app/public/js/calendarEvents.js
@@ -1,10 +1,26 @@
-$ (document).ready(function(){
+// $ (document).ready(function(){
+//   var date_input=$('input[name="date"]');
+//   var options={
+//     format: 'dd/mm/yyyy',
+//     todayHighlight: true,
+//     autoclose: true,
+//   };
+//   date_input.datepicker(options);
+//
+//   });
+
+$(document).ready(function(){
   var date_input=$('input[name="date"]');
   var options={
     format: 'dd/mm/yyyy',
     todayHighlight: true,
     autoclose: true,
   };
-  date_input.datepicker(options);
+  $("#space_datepicker").datepicker(options);
+  $("#space_datepicker").on('changeDate', function(){
+    $("#hidden_date").val(
+      $('#space_datepicker').datepicker('getFormattedDate')
+    );
+  });
 
   });
